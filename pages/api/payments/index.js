164fff2db82737485229c477bec053c5617d0ce4
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-11-11 18:00:03 +0100

    moze teraz

--- pages/api/payments/index.js
+++ pages/api/payments/index.js
@@ -3,12 +3,19 @@ export default async function handler(
     res
 ) {
 
+    const request = JSON.parse(req.body);
 
+    // Validate that the request is coming from Snipcart
+    const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.PublicToken}`)
+
+    // Return a 404 if the request is not from Snipcart
+    if (!response.ok) {
+        res.status(404).send('')
+    }
     let paymentMethodList = [{
-        id: '<payment_method_unique_id>',
-        name: '<payment_method_name>',
-        iconUrl: '<payment_method_icon_url_optional>',
-        checkoutUrl: '<payment_method_checkout_url>',
+        id: 'sleeky_pay',
+        name: 'SleekyPay',
+        checkoutUrl: `/index.html`,
     }]
     res.status(200).send(JSON.stringify(paymentMethodList))
 }
