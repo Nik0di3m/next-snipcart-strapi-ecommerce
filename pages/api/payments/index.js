ad31dcb27afa3d49df0a88bcd4df097ffa1b0f29
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-11-11 17:50:58 +0100

    test 3

--- pages/api/payments/index.js
+++ pages/api/payments/index.js
@@ -10,5 +10,5 @@ export default async function handler(
         iconUrl: '<payment_method_icon_url_optional>',
         checkoutUrl: '<payment_method_checkout_url>',
     }]
-    res.status(200).json(paymentMethodList)
+    res.status(200).send(JSON.stringify(paymentMethodList))
 }
