d1a7b7d51dea4a75d05c87516185db235ac1db0c
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-11-11 18:18:39 +0100

    testing api

--- pages/api/payments/index.js
+++ pages/api/payments/index.js
@@ -1,21 +1,22 @@
 export default async function handler(
     req,
-    res
+    res,
 ) {
 
     const request = JSON.parse(req.body);
 
-    // Validate that the request is coming from Snipcart
-    const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.PublicToken}`)
+    // // Validate that the request is coming from Snipcart
+    // const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.PublicToken}`)
 
-    // Return a 404 if the request is not from Snipcart
-    if (!response.ok) {
-        return res.status(404).send('')
-    }
-    let paymentMethodList = [{
-        id: 'sleeky_pay',
-        name: 'SleekyPay',
-        checkoutUrl: `/index.html`,
-    }]
-    res.status(200).send(JSON.stringify(paymentMethodList))
+    // // Return a 404 if the request is not from Snipcart
+    // if (!response.ok) {
+    //     return res.status(404).send('')
+    // }
+    // let paymentMethodList = [{
+    //     id: 'sleeky_pay',
+    //     name: 'SleekyPay',
+    //     checkoutUrl: `/index.html`,
+    // }]
+    console.warn(request)
+    res.status(200).send(request)
 }
