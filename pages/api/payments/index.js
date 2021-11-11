6b2ea36bf54e4ea668d7801c322f89ee95e03323
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-11-11 20:21:29 +0100

    ?

--- pages/api/payments/index.js
+++ pages/api/payments/index.js
@@ -5,19 +5,19 @@ export default async function handler(
 
     const request = req.body;
 
-    // Validate that the request is coming from Snipcart
-    const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.publicToken}`)
+    // // Validate that the request is coming from Snipcart
+    // const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.publicToken}`)
 
-    // Return a 404 if the request is not from Snipcart
-    if (!response.ok) {
-        return res.status(404).send('')
-    }
+    // // Return a 404 if the request is not from Snipcart
+    // if (!response.ok) {
+    //     return res.status(404).send('')
+    // }
     let paymentMethodList = [{
         id: 'sleeky_pay',
         name: 'SleekyPay',
         iconUrl: 'https://www.przelewy24.pl/themes/base/assets/img/logo-przelewy-24.svg',
         checkoutUrl: `/#`,
     }]
-    console.warn(paymentMethodList)
+    console.warn(JSON.stringify(paymentMethodList))
     return res.status(200).send(JSON.stringify(paymentMethodList))
 }
